174385c355233456590f0c7247bc3e9569d1503b
vivianmazeh <vivian@mazeh.io>
2024-01-01 18:02:08 -0500

    change the title to we play and cafe

--- src/app/service/course.service.ts
+++ src/app/service/course.service.ts
@@ -8,7 +8,11 @@ import { Course } from '../model/course';
 })
 export class CourseService {
 
-  private baseUrl: string ="http://easytolearn.us-east-2.elasticbeanstalk.com/learningApp/education"
+  private ProbaseUrl: string ="http://easytolearn.us-east-2.elasticbeanstalk.com/learningApp/education";
+  private localBaseUrl: string ="http://www.localhost:8080/education";
+  private s3Url: string = "http://angular-easy-to-learn-web.s3-website.us-east-2.amazonaws.com";
+  private originUrl: string = 'http://localhost:4200';
+
 
   constructor(private http: HttpClient) {}
 
@@ -18,11 +22,11 @@ export class CourseService {
       // Set up the headers to include CORS
       const headers = new HttpHeaders({
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': 'http://angular-easy-to-learn-web.s3-website.us-east-2.amazonaws.com'
+        'Access-Control-Allow-Origin': this.originUrl
         // Add any other required headers
       });
 
-    return this.http.get<Course[]>(`${this.baseUrl}/allbooks`, { headers });
+    return this.http.get<Course[]>(`${this.localBaseUrl}/allbooks`, { headers });
   
   }
   
